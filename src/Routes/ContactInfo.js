9b4eb7233717051ad3a4eb173755090fa8c5a3a3
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-08-02 16:44:48 +0200

    add component contact info

---
+++ src/Routes/ContactInfo.js
@@ -0,0 +1,13 @@
+function ContactInfo() {
+  return (
+    <div className="App__Content-section">
+      <h3 className="Content__Section-title">ContactInfo</h3>
+      <ul className="Content__Section-text">
+        <li>Marcus Lindström XXXX-XXXX</li>
+        <li>Sara Khabazbashi XXXX-XXXX</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ContactInfo;
