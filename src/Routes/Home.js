718d4d359d56b39a964647f669c2cab20c5313b8
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-08-02 16:50:24 +0200

    add component home

---
+++ src/Routes/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="App__Intro">
+      <p>Marcus and Sara are having a wedding. </p>
+      <p>Vi ser fram emot att träffa er på bröllopet!</p>
+    </div>
+  );
+}
+
+export default Home;
