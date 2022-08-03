f4b2ab3fdd90aacc7fcc274f5d4075f55c56a4be
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-08-03 07:18:27 +0200

    wip styling components

--- src/Routes/TimeAndPlace.js
+++ src/Routes/TimeAndPlace.js
@@ -1,9 +1,18 @@
 function TimeAndPlace() {
   return (
     <div className="pages__container">
-      <h3 className="pages__title">Tid och Plats</h3>
+      <h1 className="pages__title">Tid och Plats</h1>
+      <h2 className="pages__subtitle"> Adress</h2>
+      <p className="pages__infotext">
+        Skoklostervägen 16 <br /> 74694 Häggeby
+      </p>
+      <h2 className="pages__subtitle"> Tid</h2>
+      <p className="pages__infotext">15:00</p>
+      <h2 className="pages__subtitle"> Övrig information</h2>
       <p className="pages__text">
-        Skoklostervägen 16, 74694, Häggeby. The latest 15:00.
+        Ni är varmt välkomna att titta in tidigare för att ta del av det som
+        finns i området såsom bad i närliggande sjö, tennis, biljard eller bara
+        för att packa upp och ställa in er i era rum.
       </p>
     </div>
   );
