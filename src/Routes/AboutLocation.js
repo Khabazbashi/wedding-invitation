8da0786a941e50c93e0c284ec68e3c7027ba1135
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-08-04 18:35:00 +0200

    add text styling

--- src/Routes/AboutLocation.js
+++ src/Routes/AboutLocation.js
@@ -14,6 +14,12 @@ function AboutLocation() {
           13 augusti <br />
           15:00
         </p>
+        <h2 className="pages__subtitle"> Övrig information</h2>
+        <p className="pages__text">
+          Ni är varmt välkomna att titta in tidigare för att ta del av det som
+          finns i området såsom bad i närliggande sjö, tennis, biljard eller
+          bara för att packa upp och ställa in er i era rum.
+        </p>
       </div>
     </div>
   );
