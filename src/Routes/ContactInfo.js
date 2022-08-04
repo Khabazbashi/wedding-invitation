491f4b3ecba0eb06be0e156fb7a007c55914fab0
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-08-04 18:33:17 +0200

    structure of contact info page

--- src/Routes/ContactInfo.js
+++ src/Routes/ContactInfo.js
@@ -6,8 +6,27 @@ function ContactInfo() {
       </div>
       <div className="pages__body">
         <ul className="pages__list">
-          <li className="pages__infotext">Marcus Lindström 0760436199</li>
-          <li className="pages__infotext">Sara Khabazbashi 0704176182</li>
+          <li className="pages__listitem">
+            <h2 className="pages__subtitle">Marcus</h2>
+            <a className="pages__link" href="tel:0760436199">
+              0760436199
+            </a>
+            <a
+              className="pages__link"
+              href="mailto:marcus.lindstrom1@gmail.com"
+            >
+              marcus.lindstrom1@gmail.com
+            </a>
+          </li>
+          <li className="pages__listitem">
+            <h2 className="pages__subtitle">Sara</h2>
+            <a className="pages__link" href="tel:0704176182">
+              0704176182
+            </a>
+            <a className="pages__link" href="mailto:sara.khabazbashi@gmail.com">
+              sara.khabazbashi@gmail.com
+            </a>
+          </li>
         </ul>
       </div>
     </div>
